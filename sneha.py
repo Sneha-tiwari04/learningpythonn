c4fa8533dd8f574adb937b74a92d160ae7af0146
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-12 11:22:18 +0530

    file

---
+++ sneha.py
@@ -0,0 +1,42 @@
+
+1 question
+n = int(input("Enter a number: "))
+print("Even numbers are:")
+for i in range(2, n*2 + 1, 2):
+    print(i, end=" ")
+    
+    2 question
+    
+n = int(input("Enter a number: "))
+sum_even = 0
+for i in range(2, n*2 + 1, 2):
+    sum_even += i
+print("Sum of even numbers =", sum_even)
+
+3 question 
+
+n = int(input("Enter a number: "))
+product = 1
+for i in range(2, n*2 + 1, 2):
+    product *= i
+print("Multiplication of even numbers =", product)
+
+4 question 
+
+n = int(input("Enter a number: "))
+print("Odd numbers are:")
+for i in range(1, n*2, 2):
+    print(i, end=" ")
+
+5 question
+
+n = int(input("Enter a number: "))
+sum_odd = 0
+for i in range(1, n*2, 2):
+    sum_odd += i
+print("Sum of odd numbers =", sum_odd)
+
+7 question 
+n = int(input("Enter a number: "))
+for i in range(1, 11):
+    print(n, "x", i, "=", n*i)
