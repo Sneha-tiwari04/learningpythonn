bffc49954c285ae0fc2a0877bf7cdaaabcdbac96
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-13 11:17:36 +0530

    first file

---
+++ class.py
@@ -0,0 +1,48 @@
+LOGICAL OPREATOR
+# s1='python'
+# s2='language'
+# print(not s1)
+# print(s1 or s2)
+
+# s1='python'
+# print(not s1)
+
+# s2=''
+# print(not s2)
+
+IDENTITY
+is,is not
+s1='python'
+s2='Python'
+print(s1 is s2)#(compaire on the basic of memory address)
+print(s1==s2)#(compaire on the basic of value)
+
+MEMBERSHIP OPERATER ON STRING(IN,NOT IN)
+s='pyhton'
+print('N' in s)
+print('o' in s)
+
+METHODS IN STRING
+string.method
+lower()
+upper()
+swapcases()
+capilatize()
+title()
+index()
+count()
+split()
+join()
+s=' THIS IS Python CLASS'
+print(s.lower())
+print(s)
+s=' this is pyhton class'
+print(s.upper())
+print(s)
+s=' THIS IS python class'
+print(s.swapcase())
+s='this is Pyhton class'
+print(s.capitalize())
+print(s.title())
+s.
+
