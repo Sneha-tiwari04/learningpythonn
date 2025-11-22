29cf0c210f26cc5b2553c64a017e5b79958bab18
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-22 11:33:16 +0530

    file

--- a5.py
+++ a5.py
@@ -2,5 +2,13 @@
 # n=15
 # print(n)
 
-x=int(input('Enter any no :---'))
-print(x)
+# x=int(input('Enter any no :---'))
+# print(x)
+
+def fib(n):
+     a, b = 0, 1
+     while a < n:
+         print(a, end=' ')
+         a, b = b, a+b
+     print()
+     fib(1000)
