79c245d56465f858ad4c4327e4becd786e3057e5
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-22 14:42:16 +0530

    file

--- a5.py
+++ a5.py
@@ -5,10 +5,17 @@
 # x=int(input('Enter any no :---'))
 # print(x)
 
-def fib(n):
-     a, b = 0, 1
-     while a < n:
-         print(a, end=' ')
-         a, b = b, a+b
-     print()
-     fib(1000)
+# def fib(n):
+#      a, b = 0, 1
+#      while a < n:
+#          print(a, end=' ')
+#          a, b = b, a+b
+#      print()
+#      fib(1000)
+
+# n=int(input("enter a number"))
+# for i in range(n):
+#     print(i)
+
+n=int(input("enter a number"))
+print([i for i in range(n)])
