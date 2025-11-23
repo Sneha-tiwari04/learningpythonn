5678b2c3bb97c395bba4054549dd0ed0ae0bbb05
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-23 19:16:54 +0530

    file

--- else.py
+++ else.py
@@ -6,3 +6,18 @@ elif num<0:
     print("the number is negative")
 else:
     print("the number is zero")
+    
+    #write a program to check grater no amoungs these three numbers
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+z=int(input("enter a number"))
+if x>y:
+    if x>z:
+      print("greter no.is(x):",x)
+    else:
+        print("greter nomber is (z)",z)
+else:
+    if y>z:
+        print("greter number is (y)",y)
+    else:
+        print("greter number is (z)",z)
