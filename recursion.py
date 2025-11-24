d0b9d1fdf9327be51548cd9b8f2228d82bfbde49
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-24 11:12:40 +0530

    file

--- recursion.py
+++ recursion.py
@@ -8,7 +8,7 @@
     n=int(input("enter a number:"))
     natural_no(n)
     '''
-
+#*************************************************************************************************
 '''def natural_no(n):
     if n==0:
         return
@@ -18,15 +18,19 @@
     natural_no(n)
     '''
 #***********************************************************************************************
-def sum_natural(n):
-    if n == 1:      # base case
+'''def sum_natural(n):
+    if n == 1:    
         return 1
     return n + sum_natural(n - 1)
-
-# take input from user
 num = int(input("Enter a number: "))
 
-# call the function and print result
 print("Sum =", sum_natural(num))
+'''
 
-    
+def sum_natural(n):
+    if n==1:
+        return 1
+    return 2*n +sum_natural(n-1)
+n=int(input("enter a number"))
+res=sum_natural(n)
+print(res)
