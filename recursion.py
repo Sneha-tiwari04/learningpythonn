3129dceb79b8236f71c2439c32199f10ac489d70
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-24 11:19:16 +0530

    file

--- recursion.py
+++ recursion.py
@@ -26,11 +26,22 @@ num = int(input("Enter a number: "))
 
 print("Sum =", sum_natural(num))
 '''
-
-def sum_natural(n):
+#************************************************************************************************
+'''def sum_natural(n):
     if n==1:
         return 1
     return 2*n +sum_natural(n-1)
 n=int(input("enter a number"))
 res=sum_natural(n)
+print(res)'''
+
+#*************************************************************************************************
+
+def multi(n):
+    if n==1:
+        return 1
+    return n*multi(n-1)
+n=int(input("enter a number"))
+res=multi(5)
 print(res)
+
