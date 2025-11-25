8d9d74686ab9fd75360681713d1dbd0b6e6fda46
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-25 15:35:39 +0530

    file

---
+++ a7.py
@@ -0,0 +1,11 @@
+list ka plha element print karo
+'''nums=[10,20,30,40,50]
+print(nums[0])'''
+
+# list ka last element print karo
+letter=['a','b','c','d','e']
+print(letter[4])
+
+#string ka third letter print karo
+text="sneha"
+print(text[2])
