2442b8fa150a7a43bec75a4705d794530e54254f
Sneha-tiwari04 <snehatiwari7654321@gmail.com>
2025-11-26 11:32:04 +0530

    file

---
+++ if.py
@@ -0,0 +1,23 @@
+#1) list ka plha element print karo
+'''nums=[10,20,30,40,50]
+print(nums[0])'''
+
+# list ka last element print karo
+letter=['a','b','c','d','e']
+print(letter[4])
+
+#string ka third letter print karo
+text="sneha"
+print(text[2])
+
+#1) list ka plha element print karo
+'''nums=[10,20,30,40,50]
+print(nums[0])'''
+
+# list ka last element print karo
+letter=['a','b','c','d','e']
+print(letter[4])
+
+#string ka third letter print karo
+text="sneha"
+print(text[2])
